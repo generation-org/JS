2576ff844ce95df4e888a51b6ea7bf3b84aba48b
Debbie Ly <debbs.ly@gmail.com>
2020-09-09 13:23:56 -0700

    Update calculator.js with note

--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.js
@@ -13,4 +13,5 @@ function multiply(a, b){
   return a * b;
 }
 
+// When you want to use the divide or multiple function, remember to add it to the export here
 module.exports = {add: add, subtract: subtract};
